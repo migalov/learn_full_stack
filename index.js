74f09cf4db760cf5a22c40f1b49bed9010ee3b7d
Michel Muniagiseny <mafri.michel@gmail.com>
2024-03-17 21:52:56 +0300

    Step 7. Refactoring and hide variables to .env

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import express, { json } from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config({ path: `.env` });
 
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
-import UserModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+// import { register, login, getMe } from './controllers/UserController.js';
+import * as UserController from './controllers/UserController.js';
 
-
-mongoose.connect(`mongodb+srv://admin:qqqqqq@cluster0.x2zrya7.mongodb.net/blog?retryWrites=true&w=majority`)
+mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log("Connect DB success"))
     .catch((err) => console.log('DB error', err));
 
@@ -18,127 +17,6 @@ const app = express();
 
 app.use(json());
 
-app.post('/auth/login', async (req, res) => {
-    try {
-        const user = await UserModel.findOne({
-            email: req.body.email,
-        });
-
-        if (!user) {
-            return req.status(404).json({
-                message: "Пользователь не найден" // Не уточнять в реальных проектах
-            });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-        if (!isValidPass) {
-            return req.status(403).json({
-                message: "Неверный логин или пароль"
-            })
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123', 
-            {
-                expiresIn: '30d',
-            }
-        );
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token
-        });
-    }
-
-    catch (err)  {
-        console.log(err);
-        res.status(400).json({
-            message: "Не удалось авторизоваться!"
-        })
-    }
-});
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const   errors = validationResult(req),
-            password = req.body.password,
-            salt = await bcrypt.genSalt(10),
-            hash = await bcrypt.hash(password, salt)
-
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
-        }
-        
-
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash
-        })
-
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123', 
-            {
-                expiresIn: '30d',
-            }
-        );
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token
-        });
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Не удалось зарегистрироваться!"
-        })
-    }
-    
-})
-
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId);
-
-        if (!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            });
-        }
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json(userData);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Нет доступа"
-        })
-    }
-})
-
-app.listen(4444, (err) => {
-    
-    if (err) {
-        return console.log(err);
-    }
-
-    console.log('Server OK!');
-})
+app.post('/auth/login', UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
