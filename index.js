2d5b5b4b9060c16019e83e7adc46953f6026b833
Michel Muniagiseny <mafri.michel@gmail.com>
2024-03-17 19:47:18 +0300

    Step 4. Registration.

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 import express, { json } from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
-mongoose.connect(`mongodb+srv://dremor93:Ltwu3bszWXE4zjWD@cluster0.x2zrya7.mongodb.net/?retryWrites=true&w=majority`)
+import UserModel from './models/User.js'
+
+mongoose.connect(`mongodb+srv://admin:qqqqqq@cluster0.x2zrya7.mongodb.net/blog?retryWrites=true&w=majority`)
     .then(() => console.log("Connect DB success"))
-    .catch((err) => console.log('DB error', err))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(json());
 
-app.post('/auth/register', registerValidation, function (req, res) {
-    const errors = validationResult(req);
-    if (errors.isEmpty) {
-        return res.status(400).json(errors.array());
-    }
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const   errors = validationResult(req),
+            password = req.body.password,
+            salt = await bcrypt.genSalt(10),
+            hash = await bcrypt.hash(password, salt)
 
-    res.json({
-        success: true
-    })
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+        
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash
+        })
+
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret', 
+            {
+                expiresIn: '30d',
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться!"
+        })
+    }
     
 })
 
